7a342ae9e3c65ff8f9238803181f0e99e1ce93c5
minas1986 <30533267+minas1986@users.noreply.github.com>
2020-08-25 09:55:10 +0300

    Factories Users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,9 +1,10 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
 
 use App\Models\User;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 use Illuminate\Support\Str;
 
 /*
@@ -18,12 +19,15 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+   $active = $faker->boolean;
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-        'status' => User::STATUS_ACTIVE
+        'verify_token' => $active ? null : Str::uuid(),
+        'status' => $active ? User::STATUS_ACTIVE : User::STATUS_WAIT,
     ];
 });
