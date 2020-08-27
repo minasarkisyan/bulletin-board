de897ba83a53a8356f64c9cb992f4b56599b12dc
minas1986 <30533267+minas1986@users.noreply.github.com>
2020-08-27 09:38:00 +0300

    Change locale in lang:ru

--- app/Models/Adverts/Category.php
+++ app/Models/Adverts/Category.php
@@ -5,6 +5,17 @@ namespace App\Models\Adverts;
 use Illuminate\Database\Eloquent\Model;
 use Kalnoy\Nestedset\NodeTrait;
 
+/**
+ * @property int $id	 * @property int $id
+ * @property string $name	 * @property string $name
+ * @property string $slug	 * @property string $slug
+ * @property int|null $parent_id	 * @property int|null $parent_id
+ **
+ * @property int $depth	 * @property int $depth
+ * @property Category $parent	 * @property Category $parent
+ * @property Category[] $children	 * @property Category[] $children
+ * @property Attribute[] $attributes
+ */
 class Category extends Model
 {
     use NodeTrait;
@@ -14,4 +25,9 @@ class Category extends Model
     public $timestamps = false;
 
     protected $fillable = ['name', 'slug', 'parent_id'];
+
+    public function attributes()
+    {
+        return $this->hasMany(Attribute::class, 'category_id', 'id');
+    }
 }
---
+++ database/migrations/2020_08_27_062205_create_attributes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attributes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('advert_categories')->onDelete('CASCADE');
+            $table->string('name');
+            $table->string('type');
+            $table->boolean('required');
+            $table->json('variants');
+            $table->integer('sort');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attributes');
+    }
+}
