8f81925b6d5d4fbdf23dfc21ef2e081a5fb3153a
minas1986 <30533267+minas1986@users.noreply.github.com>
2020-08-27 09:07:17 +0300

    bug-fix class=active

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,4 +7,7 @@
         <li class="nav-item"><a class="nav-link" href="{{ route('admin.adverts.categories.index') }}">Categories</a></li>
         <li class="nav-item"><a class="nav-link" href="{{ route('admin.users.index') }}">Users</a></li>
     </ul>
+    <div class="col">
+        <p>Home page</p>
+    </div>
 @endsection
